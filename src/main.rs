74b24248be8d58262c17aaf254023ea4523a5ce8
Pedro Martin <pedro.martin@red-badger.com>
2020-03-02 14:31:54 +0000

    second exercise

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
 use std::fs::File;
+use std::io;
+use std::io::prelude::*;
 
-fn main() {
-    let f = File::open("src/data/content.txt");
+fn main() -> Result<(), io::Error> {
+    let open_file = File::open("src/data/content.txt");
 
-    match f {
-        Ok(_) => println!("💥"),
-        Err(error) => println!("Open failed {:}", error),
-    }
+    let mut file = match open_file {
+        Ok(f) => f,
+        Err(e) => panic!("Error: {}", e),
+    };
+
+    let mut buffer = String::new();
+    let contents = file.read_to_string(&mut buffer);
+
+    match contents {
+        Ok(file_content) => file_content,
+        Err(e) => panic!("Error: {}", e),
+    };
+
+    println!("{}", buffer);
+
+    Ok(())
 }
