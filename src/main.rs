ba84530e9bb8774010230a3cff423a48b7aa27d7
Pedro Martin <pedro.martin@red-badger.com>
2020-03-04 11:16:52 +0000

    fizzbuzz

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,12 @@
-use std::collections::VecDeque;
-use std::io::prelude::*;
-use std::net::{TcpListener, TcpStream};
+fn main() {
+    let numbers = 0..101;
 
-fn handle_client(mut stream: TcpStream, vec: &mut VecDeque<String>) -> Result<(), std::io::Error> {
-    let mut buffer = String::new();
-    stream.read_to_string(&mut buffer)?;
-    stream.write_all(buffer.as_bytes())?;
-
-    if !buffer.trim().is_empty() {
-        println!("{} is stored 📬", buffer);
-        vec.push_back(buffer);
-    } else {
-        let msg = vec.pop_back();
-
-        match msg {
-            Some(msg) => stream.write(msg.as_bytes()),
-            None => stream.write(b"no messages"),
-        };
+    for number in numbers {
+        match number {
+            number if number % 5 == 0 && number % 3 == 0 => println!("{} fizzbuzz", number),
+            number if number % 3 == 0 => println!("{} fizz", number),
+            number if number % 5 == 0 => println!("{} buzz", number),
+            _ => println!("{}", number),
+        }
     }
-
-    Ok(())
-}
-
-fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080")?;
-
-    let mut stored_messages: VecDeque<String> = VecDeque::new();
-
-    // accept connections and process them serially
-    for stream in listener.incoming() {
-        handle_client(stream?, &mut stored_messages).unwrap();
-    }
-    Ok(())
 }
